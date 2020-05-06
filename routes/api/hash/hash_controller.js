6c39b115ddde08b867b0367aaad20bacdd4eb303
Park <bennp3038@gmail.com>
2020-05-06 16:28:13 +0900

    chatList
    uID를 get하여 chatList 불러오기

--- routes/api/hash/hash_controller.js
+++ routes/api/hash/hash_controller.js
@@ -1,15 +1,34 @@
 const fs = require("fs");
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const _url = require('url');
 
-//const bodyParser = require("body-parser");
-
-
-
-
-
-const hashList = [];
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
 
 exports.hashAdd = (req, res) =>{
+    const hashList = [];
     console.log(req.body);
     hashList.push(req.body);
     res.json(hashList);
 }
+
+exports.ChatList = (req, res) =>{
+    let uID = req.query.id;
+    let hashList = req.query.hashList;
+    let chatList;
+    if(hashList == null){
+        connection.query("select * from ChatList where chatID IN (select chatID from ChatMember where uID =" + uID + ");", function(err, row, fields){
+            res.send(row);
+            console.log(row);
+           
+        });
+    }
+}   
+
--- routes/api/hash/index.js
+++ routes/api/hash/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const hash_controller = require('./hash_controller');
 
 
-
-router.post('/hashAdd', hash_controller.hashAdd)
+router.post('/hashAdd', hash_controller.hashAdd);
+router.get('/chatList', hash_controller.ChatList);
 
 module.exports = router;
